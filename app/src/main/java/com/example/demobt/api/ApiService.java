923abf892965a29dd90d406c5132d877586adec0
BookApp <lethanh27112002@gmail.com>
2023-08-10 03:17:40 +0700

    DemoBT

---
+++ app/src/main/java/com/example/demobt/api/ApiService.java
@@ -0,0 +1,36 @@
+package com.example.demobt.api;
+
+import com.example.demobt.models.Comic;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface ApiService {
+    String BASE_URL = "https://64d3d00867b2662bf3dcc154.mockapi.io/";
+    ApiService instance = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService.class);
+    @GET("comic")
+    Call<List<Comic>> getAllComic();
+
+    @POST("comic")
+    Call<Comic> addComic(@Body Comic comic);
+
+    @PUT("comic/{id}")
+    Call<Comic> editComic(@Path("id") String id, @Body Comic comic);
+
+    @DELETE("comic/{id}")
+    Call<Void> deleteComic(@Path("id") String id);
+
+}
